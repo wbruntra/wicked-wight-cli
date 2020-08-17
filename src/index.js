fe60675ff267cf627e6e02ed960bcf28fa6faac7
William Bruntrager <w.bruntrager@gmail.com>
2020-08-17 11:27:36 +0200

    Replace yargs with commander

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const wordlist = require('./data/got-long-words.json')
 const { generatePhrases } = require('./utils')
-var yargs = require('yargs')
-const argv = yargs
-  .usage('Usage: $0 -n [num] -l [num] [-m]')
-  .help('h')
-  .alias('h', 'help')
-  .example('$0 --num 8 --len 5 --mixed', 'Get 8 passphrases of length 5, mixing words and numbers')
-  .describe('n', 'Number of phrases to generate')
-  .describe('l', 'Length of passphrase')
-  .describe('m', 'Generate mixed (numbers and words) passphrase')
-  .alias('m', 'mixed')
-  .alias('n', 'num')
-  .alias('l', 'len')
-  .default('n', 5)
-  .default('l', 5).argv
+const { program } = require('commander')
+
+program
+  .option('-m, --mixed', 'Use mix of words and numbers', false)
+  .option('-n, --num <number>', 'Number of phrases to create', '5')
+  .option('-l, --len <number>', 'Length of each phrase', '5')
+
+program.parse(process.argv)
 
 const run = () => {
-  const phrases = generatePhrases(argv.n, argv.l, wordlist, argv.m ? 'mixed' : 'words')
+  const phrases = generatePhrases(
+    parseInt(program.num),
+    parseInt(program.len),
+    wordlist,
+    program.mixed ? 'mixed' : 'words'
+  )
   let longest = Math.max(...phrases.map((phrase) => phrase.length))
   console.log('='.repeat(65))
   phrases.forEach((p) => {
@@ -29,4 +28,6 @@ const run = () => {
   return phrases
 }
 
-run()
+if (require.main === module) {
+  run()
+} 
